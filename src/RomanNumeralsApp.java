91fecbb00138e91fff29650401e947c213bfd0b6
maya <mayazaha2@gmail.com>
2022-06-19 13:29:39 +0300

    Implemented generate function

--- src/RomanNumeralsApp.java
+++ src/RomanNumeralsApp.java
@@ -3,12 +3,20 @@ import services.RomanNumeralGenerator;
 public class RomanNumeralsApp {
     public static void main(String[] args){
         RomanNumeralGenerator numeralGenerator = new RomanNumeralGenerator();
-        System.out.println(numeralGenerator.generate(100));
+        System.out.println("Testing generate function");
+        System.out.println("Testing invalid input 5000");
         try {
             System.out.println(numeralGenerator.generate(5000));
         }
         catch (IllegalArgumentException exception){
             System.out.println(exception.getMessage());
         }
+        System.out.println("3997 is: " + numeralGenerator.generate(3997));
+        System.out.println("2345 is: " + numeralGenerator.generate(2345));
+        System.out.println("1699 is: " + numeralGenerator.generate(1699));
+        System.out.println("723 is: " + numeralGenerator.generate(723));
+        System.out.println("102 is: " + numeralGenerator.generate(102));
+        System.out.println("43 is: " + numeralGenerator.generate(43));
+        System.out.println(numeralGenerator.parse("XX"));
     }
 }
--- src/services/RomanNumeralGenerator.java
+++ src/services/RomanNumeralGenerator.java
@@ -1,12 +1,86 @@
 package services;
 
+import java.util.Map;
+
 public class RomanNumeralGenerator {
+    private static final Map<Character,Integer> characterIntegerMap = Map.of(
+            'I',1,
+            'V',5,
+            'X', 10,
+            'L', 50,
+            'C', 100,
+            'D', 500,
+            'M', 1000
+    );
+
+    private Boolean validateRoman(){
+        
+        return true;
+    }
+
     public String generate(Integer number){
         if(number > 3999 || number < 0)
             throw new IllegalArgumentException("Number must be between 1 and 3999");
-        return "IX";
-    };
+        StringBuilder romanNumber = new StringBuilder();
+        while(number > 0){
+            if(number >= 1000) {
+                romanNumber.append("M".repeat(number / 1000));
+                number %= 1000;
+            }
+            else if(number >= 900){
+                romanNumber.append("CM");
+                number -= 900;
+            }
+            else if(number >= 500){
+                romanNumber.append("D");
+                number -= 500;
+            }
+            else if(number >= 400){
+                romanNumber.append("CD");
+                number -= 400;
+            }
+            else if(number >= 100){
+                romanNumber.append("C".repeat(number / 100));
+                number %= 100;
+            }
+            else if(number >= 90){
+                romanNumber.append("XC");
+                number -= 90;
+            }
+            else if(number >= 50){
+                romanNumber.append("L");
+                number -= 50;
+            }
+            else if(number >= 40){
+                romanNumber.append("XL");
+                number -= 40;
+            }
+            else if(number >= 10){
+                romanNumber.append("X".repeat(number / 10));
+                number %= 10;
+            }
+            else if(number == 9){
+                romanNumber.append("IX");
+                number = 0;
+            }
+            else if(number >= 5){
+                romanNumber.append("V");
+                number -= 5;
+            }
+            else if(number == 4){
+                romanNumber.append("IV");
+                number = 0;
+            }
+            else{
+                romanNumber.append("I".repeat(number));
+                number = 0;
+            }
+        }
+
+        return romanNumber.toString();
+    }
+
     public Integer parse(String roman){
-        return 4;
-    };
+        return characterIntegerMap.get('X');
+    }
 }
